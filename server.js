7be060f67bc7aeea6604af311ff7b64a8bd650bd
josh-stevens <jcstevens12@outlook.com>
2015-08-31 14:21:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var connection = mysql.createConnection({
           host     : 'localhost',
           user     : 'root',
           password : '',
-          database: 'strength_tracker'
+          database: ''
         });
 
 require('./config/passport')(passport, connection);
