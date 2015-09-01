3af55b949e9aa34079cde155e594a0e29719ccb5
josh-stevens <jcstevens12@outlook.com>
2015-09-01 08:51:40 -0500

    Fixed FB email error
    Fixed an error that allowed a Facebook user to sign up multiple times

--- config/passport.js
+++ config/passport.js
@@ -83,7 +83,7 @@ module.exports = function(passport, connection) {
     },
     function(accessToken, refreshToken, profile, done) {
         process.nextTick(function(){
-          connection.query("SELECT * FROM `users` WHERE `email` = ?", [profile.emails[0].value], function(err, rows){
+          connection.query("SELECT * FROM `users` WHERE `facebook_email` = ?", [profile.emails[0].value], function(err, rows){
             if(err)
               return done(err);
             if(rows.length)
